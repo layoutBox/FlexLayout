175ce072b68b1c879cf74b4be07d7fe8a7b94ed5
Luc Dion <ldion@mirego.com>
2017-12-20 08:51:02 -0500

    Update swiftlint

--- Example/FlexLayoutSample/UI/Common/Stylesheet.swift
+++ Example/FlexLayoutSample/UI/Common/Stylesheet.swift
@@ -13,5 +13,7 @@
 import UIKit
 
 extension UIColor {
-    class var flexLayoutColor: UIColor { return UIColor(red: 90.0 / 255.0, green: 171.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0) }
+    class var flexLayoutColor: UIColor {
+        return UIColor(red: 90.0 / 255.0, green: 171.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0)
+    }
 }
