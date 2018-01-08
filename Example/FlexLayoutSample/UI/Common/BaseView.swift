df8c3a4fdb1442bd524c68efced588c0fc30ba6c
Luc Dion <ldion@mirego.com>
2018-01-08 17:55:30 -0500

    Fix baseview

--- Example/FlexLayoutSample/UI/Common/BaseView.swift
+++ Example/FlexLayoutSample/UI/Common/BaseView.swift
@@ -26,6 +26,15 @@ class BaseView: UIView {
         }
     }
     
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func setSafeArea(_ safeArea: UIEdgeInsets) {
         guard safeArea != self.safeArea else { return }
         legacySafeArea = safeArea
--- Example/FlexLayoutSample/UI/Common/BaseViewController.swift
+++ Example/FlexLayoutSample/UI/Common/BaseViewController.swift
@@ -13,6 +13,17 @@
 import UIKit
 
 class BaseViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
@@ -23,31 +34,3 @@ class BaseViewController: UIViewController {
     }
 }
 
-class BaseView: UIView {
-    private var legacySafeArea: UIEdgeInsets = .zero
-    
-    // safeArea returns UIView.safeAreaInsets for iOS 11+ or
-    // an UIEdgeInsets initialized with the UIViewController's topLayoutGuide and
-    // bottomLayoutGuide for other iOS versions.
-    var safeArea: UIEdgeInsets {
-        if #available(iOS 11.0, *) {
-            return safeAreaInsets
-        } else {
-            return legacySafeArea
-        }
-    }
-    
-    func setSafeArea(_ safeArea: UIEdgeInsets) {
-        guard safeArea != self.safeArea else { return }
-        legacySafeArea = safeArea
-        safeAreaDidChange()
-    }
-    
-    func safeAreaDidChange() {
-        setNeedsLayout()
-    }
-    
-    override func safeAreaInsetsDidChange() {
-        safeAreaDidChange()
-    }
-}
