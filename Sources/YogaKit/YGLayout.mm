f674a0c541f8623d84380144e1f9815e96767a44
Aleksey Zinchenko <a.zintus@gmail.com>
2018-02-27 15:21:25 +0800

    Apply https://github.com/zintus/FlexLayout/commit/6fd725726b5dc09f57c202ac7e9115e091650f34

--- Sources/YogaKit/YGLayout.mm
+++ Sources/YogaKit/YGLayout.mm
@@ -388,7 +388,7 @@ static void YGAttachNodesFromViewHierachy(UIView *const view)
 
     NSMutableArray<UIView *> *subviewsToInclude = [[NSMutableArray alloc] initWithCapacity:view.subviews.count];
     for (UIView *subview in view.subviews) {
-      if (subview.yoga.isEnabled && subview.yoga.isIncludedInLayout) {
+      if (subview.yoga.isIncludedInLayout) {
         [subviewsToInclude addObject:subview];
       }
     }
