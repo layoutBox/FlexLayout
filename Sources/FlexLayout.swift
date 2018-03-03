c8fc426051e1cbf27d9c881c45d138a3af6ce1e0
Aleksey Zinchenko <a.zintus@gmail.com>
2018-03-03 20:37:13 +0800

    Make view publicly readable

--- Sources/FlexLayout.swift
+++ Sources/FlexLayout.swift
@@ -31,7 +31,7 @@ public final class Flex {
     /**
      Flex items's UIView.
     */
-    private weak var view: UIView?
+    private(set) weak var view: UIView?
     private let yoga: YGLayout
     
     /**
