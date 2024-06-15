d4478fb22f502b14dcc15e75aa41b5487a817515
heoblitz <wone2287@gmail.com>
2024-06-15 14:58:14 +0900

    Add static position inset tests

--- Tests/FlexLayoutTests/InsetTests.swift
+++ Tests/FlexLayoutTests/InsetTests.swift
@@ -116,4 +116,52 @@ final class InsetTests: XCTestCase {
         rootFlexContainer.flex.layout()
         XCTAssertEqual(aView.frame, CGRect(x: 45.0, y: 45.0, width: 310.0, height: 310.0))
     }
+    
+    func test_adjust_the_aView_size_and_relative_static_with_left() {
+        rootFlexContainer.flex.define { flex in
+            flex.addItem(aView).position(.static).grow(1).left(10)
+        }
+        rootFlexContainer.flex.layout()
+        XCTAssertEqual(aView.frame, CGRect(x: 0.0, y: 0.0, width: 400.0, height: 400.0))
+    }
+    
+    func test_adjust_the_aView_size_and_static_position_with_top() {
+        rootFlexContainer.flex.define { flex in
+            flex.addItem(aView).position(.static).grow(1).top(10)
+        }
+        rootFlexContainer.flex.layout()
+        XCTAssertEqual(aView.frame, CGRect(x: 0.0, y: 0.0, width: 400.0, height: 400.0))
+    }
+    
+    func test_adjust_the_aView_size_and_static_position_with_right() {
+        rootFlexContainer.flex.define { flex in
+            flex.addItem(aView).position(.static).grow(1).right(10)
+        }
+        rootFlexContainer.flex.layout()
+        XCTAssertEqual(aView.frame, CGRect(x: 0.0, y: 0.0, width: 400.0, height: 400.0))
+    }
+    
+    func test_adjust_the_aView_size_and_static_position_with_bottom() {
+        rootFlexContainer.flex.define { flex in
+            flex.addItem(aView).position(.static).grow(1).bottom(10)
+        }
+        rootFlexContainer.flex.layout()
+        XCTAssertEqual(aView.frame, CGRect(x: 0.0, y: 0.0, width: 400.0, height: 400.0))
+    }
+    
+    func test_adjust_the_aView_size_and_static_position_with_horizontally() {
+        rootFlexContainer.flex.define { flex in
+            flex.addItem(aView).position(.static).grow(1).horizontally(10)
+        }
+        rootFlexContainer.flex.layout()
+        XCTAssertEqual(aView.frame, CGRect(x: 0.0, y: 0.0, width: 400.0, height: 400.0))
+    }
+    
+    func test_adjust_the_aView_size_and_static_position_with_vertically() {
+        rootFlexContainer.flex.define { flex in
+            flex.addItem(aView).position(.static).grow(1).vertically(10)
+        }
+        rootFlexContainer.flex.layout()
+        XCTAssertEqual(aView.frame, CGRect(x: 0.0, y: 0.0, width: 400.0, height: 400.0))
+    }
 }
