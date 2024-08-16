e7d558d2f76a356617e5710eb7eb7b503ad77edd
heoblitz <wone2287@gmail.com>
2024-08-16 12:47:29 +0900

    Add SPM platform setting

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
   name: "FlexLayout",
+  platforms: [
+    .iOS(.v12)
+  ],
   products: [
     .library(name: "FlexLayout", targets: ["FlexLayout"]),
   ],
