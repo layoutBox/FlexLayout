1500ac63372a3926a6297d682df3a988696ec46b
heoblitz <wone2287@gmail.com>
2025-07-20 15:17:15 +0900

    Update SPM deployment target to iOS 13

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "FlexLayout",
   platforms: [
-    .iOS(.v12),
+    .iOS(.v13),
   ],
   products: [
     .library(name: "FlexLayout", targets: ["FlexLayout"]),
